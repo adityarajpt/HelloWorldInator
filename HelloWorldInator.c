0bc579a11db0ed66913a3b8993d3f50c3da7fd33
idks9 <109749154+idks9@users.noreply.github.com>
2023-04-12 17:41:56 -0300

    Add C

---
+++ HelloWorldInator.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+// I made this in my phone so the code is poorly written
+// don't worry I tested it before submitting it
+
+int uncursableHello() {
+        printf("hello world");
+        return 0;
+}
+
+int HelloWorldInator(){
+    return &uncursableHello;
+}
+
+int main()
+{
+    int uncursableHelloF = HelloWorldInator();
+    return 0;
+}
