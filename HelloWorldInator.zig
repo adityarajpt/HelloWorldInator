4aa99c9eb290d323435e52ea3bc3dbe7afea9070
Adam Dimitry Enzo Ambrosino <52578762+A-D-E-A@users.noreply.github.com>
2023-04-13 14:00:35 +0200

    Remove main function in zig implementation

--- HelloWorldInator.zig
+++ HelloWorldInator.zig
@@ -1,15 +1,10 @@
 const std = @import("std");
 
-pub fn main() !void {
-    var hello_world_fn = hello_world_inator();
-    try hello_world_fn();
-}
-
 fn hello_world() !void {
     var stdout_file = try std.io.getStdOut();
     try stdout_file.write("Hello, World!");
 }
 
-fn hello_world_inator() @typeOf(hello_world) {
+pub fn hello_world_inator() @typeOf(hello_world) {
     return hello_world;
 }
